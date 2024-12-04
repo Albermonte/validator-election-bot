6cba9ff19fe8d865bf03f0c4dc4cad84ab9e972e
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2024-12-04 09:11:26 -0500

    chore: multiple improvements Return message when no slots Added money and remove command

--- bot.ts
+++ bot.ts
@@ -107,6 +107,37 @@ bot.command("status", async (ctx) => {
   await slotsPerValidator(validator.value as string, chatId, electionBlock);
 });
 
+bot.command("money", async (ctx) => {
+  const chatId = ctx.chat.id;
+  const validator = await kv.get([chatId, "address"]);
+  if (!validator.value) {
+    await ctx.reply("No address set. Use /start to set one.");
+    return;
+  }
+  const { data, error } = await client.blockchain.getValidatorByAddress(validator.value as string);
+  if (error) {
+    console.error(error);
+    ctx.reply("Unable to get the info right now, please try again later.");
+    return;
+  }
+  const { rewardAddress } = data;
+  const { USD, NIM } = await getRewardsFromValidator(rewardAddress);
+  await ctx.reply(`Validator total rewards:\n <b>${NIM} NIM</b>\n <b>${USD} USD</b>`, { parse_mode: "HTML" });
+});
+
+bot.command("remove", async (ctx) => {
+  const chatMember = await ctx.chatMembers.getChatMember(
+    ctx.chat.id,
+    ctx.from?.id,
+  );
+  if (chatMember.status !== "creator" && chatMember.status !== "administrator") {
+    await ctx.reply("You need to be an admin to use this bot.");
+    return;
+  }
+  await kv.delete([ctx.chat.id, "address"]);
+  await ctx.reply("Address removed.");
+});
+
 bot.catch((err) => console.error(err));
 bot.start({
   allowed_updates: ["chat_member", "message"],
@@ -140,33 +171,42 @@ async function slotsPerValidator(address: string, chatId: number, block: Block) 
   const { slots } = block
 
   const assignedSlot = slots.find(slot => slot.validator === address);
-  if (!assignedSlot)
-    return;
-
   const { data, error } = await client.blockchain.getValidatorByAddress(address);
   if (error) {
     console.error(error);
     return
   }
-
-  const { numSlots } = assignedSlot;
-  console.log(`Validator ${address} has been assigned ${numSlots} slot${numSlots === 1 ? '' : 's'}.`);
-  await bot.api.sendMessage(chatId, `Validator <code>${address}</code> has been assigned <b>${numSlots} slot${numSlots === 1 ? '' : 's'}</b> in epoch ${block.epoch}`, { parse_mode: "HTML" });
+  if (assignedSlot) {
+    const { numSlots } = assignedSlot;
+    console.log(`Validator ${address} has been assigned ${numSlots} slot${numSlots === 1 ? '' : 's'}.`);
+    await bot.api.sendMessage(chatId, `Validator <code>${address}</code> has been assigned <b>${numSlots} slot${numSlots === 1 ? '' : 's'}</b> in epoch ${block.epoch}`, { parse_mode: "HTML" });
+  } else {
+    console.log(`Validator ${address} has not been assigned any slots.`);
+    await bot.api.sendMessage(chatId, `Validator <code>${address}</code> has not been assigned any slots in epoch ${block.epoch} 🥲`, { parse_mode: "HTML" });
+  }
 
   const { rewardAddress } = data;
+  const { USD, NIM } = await getRewardsFromValidator(rewardAddress);
+  console.log(`Validator ${address} has a balance of ${NIM} NIM (${USD} USD)`);
+  await bot.api.sendMessage(chatId, `Validator total rewards:\n <b>${NIM} NIM</b>\n <b>${USD} USD</b>`, { parse_mode: "HTML" });
+}
+
+async function getRewardsFromValidator(rewardAddress: string) {
   const { data: account, error: accountError } = await client.blockchain.getAccountByAddress(rewardAddress, { withMetadata: false });
   const { data: staker, error: stakerError } = await client.blockchain.getStakerByAddress(rewardAddress);
   if (accountError || stakerError) {
     console.error({ accountError, stakerError });
-    return;
+    return { USD: 0, NIM: 0 };
   }
+  
   const balance = (account.balance + staker.balance) / 1e5;
+  const NIM = Math.round(balance * 100) / 100;
+  
   const { nim: { usd: price } } = await getExchangeRates([CryptoCurrency.NIM], [FiatCurrency.USD]);
   if (!price)
-    return;
-  const USD = Math.round((balance * price + Number.EPSILON) * 100) / 100;
-  const NIM = Math.round(balance * 100) / 100;
+    return { USD: 0, NIM };
 
-  console.log(`Validator ${address} has a balance of ${NIM} NIM (${USD} USD)`);
-  await bot.api.sendMessage(chatId, `Validator total rewards:\n <b>${NIM} NIM</b>\n <b>${USD} USD</b>`, { parse_mode: "HTML" });
+  const USD = Math.round((balance * price + Number.EPSILON) * 100) / 100;
+
+  return { USD, NIM };
 }
